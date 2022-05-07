2f10527f88994d62f49b7a4e68a26494ba2851cc
ertem0 <35893360+ertem0@users.noreply.github.com>
2022-05-08 00:01:18 +0100

    connection to the server

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 const express = require("express")
 
 const app = express();
+const { Pool, Client } = require("pg");
+const credentials = require("./credentials.js");
+
+async function poolDemo() {
+    const pool = new Pool(credentials);
+    const now = await pool.query("SELECT NOW()");
+    await pool.end();
+
+    return now
+}
+
+(async () => {
+    const now = await poolDemo();
+    console.log(now.rows[0].now);
+})();
 
 //start server
 const port = 5000;
