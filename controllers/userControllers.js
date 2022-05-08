d6b5edb6a6ff0cb1edf52d75bc6ac7d8c817662c
Rodrigo Santos Figueiredo <rodres2002@gmail.com>
2022-05-08 01:32:49 +0100

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const jwt = require('jsonwebtoken');
 
+
 //definir as funcoes (VIEWS)
 module.exports = {
     login: async(req, res, next)=>{
-        return res.status(200).send()
+        const nome = req.body.name;
+        const pass = req.body.password;
+
+        return res.status(200).json({nome: nome, password: pass})
     },
     register: async(req, res, next)=>{
         return res.status(200).send()
