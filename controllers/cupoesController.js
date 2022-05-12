739fbf309dd6228416d66c566ee9df8d9120eaa1
Rodrigo Santos Figueiredo <rodres2002@gmail.com>
2022-05-12 20:33:22 +0100

    cenas do admin e ficheiro de cupoes

---
+++ controllers/cupoesController.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const jwt = require('jsonwebtoken');
+const pool  = require("../connection")
+
+module.exports{
+    createCampanha: async()=>{
+        
+    }
+}
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -51,99 +51,105 @@ module.exports = {
         const tokeninfo = jwt.verify(tokenheader, '123456')
         console.log(tokeninfo)
         
-        await pool.query('SELECT users_username FROM administrador WHERE users_username = $1',[tokeninfo.username], async(error, result)=>{
+        
+         pool.query('SELECT users_username FROM administrador WHERE users_username = $1',[tokeninfo.username], async(error, result)=>{
             if (error){
                 throw error
             }
+            
             if(result.rows[0] === undefined && (type === "administrador" || type === "vendedor")){
+                
                 return res.status(500).send()
+                
             }
+        
+        
+            //gets the table where the username is
+            await pool.query('SELECT username FROM users WHERE username = $1',[nome], async(error, result)=>{
+                if (error) {
+                    
+                    throw error
+                }
+                
+                //check if the return of the select query is empty
+                if(result.rows[0] == undefined){
+                    
+                    //add user in the users table
+                    await pool.query('INSERT INTO users (username, password, email) VALUES ($1, $2, $3)', [nome, pass, email] , async(error, result)=>{
+
+                        if (error) {
+                            throw error
+                        }
+
+                        if(type === "comprador"){
+                            // get the values for the comprador
+                            const morada = req.body.morada
+                            const cartao = req.body.cartao
+                            const NIF = req.body.nif
+                            
+                            //add comprador in comprador table
+                            await pool.query('INSERT INTO comprador (morada, cartao, users_username, nif) VALUES ($1, $2, $3, $4)',[morada,  cartao, nome,NIF], (error, result)=>{
+        
+                                if (error) {
+                                    throw error
+                                }
+                                
+                                const accessToken = jwt.sign(user, '123456');
+                                
+                                return res.status(200).json({
+                                    response: "comprador added",
+                                    access_token: accessToken
+                                })
+                            });
+        
+                        }
+                        const tokenheader = req.headers.authorization  
+                        //supostamente devia decodificar o token mas nao funciona
+                        const tokeninfo = jwt.verify(tokenheader, '123456')
+                        //add a new vendedor to vendedor table
+                        if(type === "vendedor"){
+                            const morada = req.body.morada
+                            const NIF = parseInt(req.body.nif) 
+
+
+                            await pool.query('INSERT INTO vendedor (morada, users_username, nif) VALUES ($1, $2, $3)',[morada,  nome,NIF], (error, result)=>{
+        
+                                if (error) {
+                                    throw error
+                                }
+                                const accessToken = jwt.sign(user, '123456');
+                                return res.status(200).json({
+                                    response: "vendedor added",
+                                    access_token: accessToken
+                                })
+                            });
+                        }
+                        //add a new 
+                        if(type === "administrador"){
+                            await pool.query('INSERT INTO administrador (users_username) VALUES ($1)',[nome], (error, result)=>{
+        
+                                if (error) {
+                                    throw error
+                                }
+                                const accessToken = jwt.sign(user, '123456');
+                                return res.status(200).json({
+                                    response: "administrador added",
+                                    access_token: accessToken
+                                })
+                            });
+                        }
+                    });
+
+                    
+
+                }
+                
+                else{
+                    
+                    return res.status(200).json({resultado: "user already exists", user: result.rows[0].username})
+                }
+            })
         }) 
-        //gets the table where the username is
-        await pool.query('SELECT username FROM users WHERE username = $1',[nome], async(error, result)=>{
-            if (error) {
-                
-                throw error
-            }
-            
-            //check if the return of the select query is empty
-            if(result.rows[0] == undefined){
-                
-                //add user in the users table
-                await pool.query('INSERT INTO users (username, password, email) VALUES ($1, $2, $3)', [nome, pass, email] , async(error, result)=>{
-
-                    if (error) {
-                        throw error
-                    }
-
-                    if(type === "comprador"){
-                        // get the values for the comprador
-                        const morada = req.body.morada
-                        const cartao = req.body.cartao
-                        const NIF = req.body.nif
-                        
-                        //add comprador in comprador table
-                        await pool.query('INSERT INTO comprador (morada, cartao, users_username, nif) VALUES ($1, $2, $3, $4)',[morada,  cartao, nome,NIF], (error, result)=>{
-    
-                            if (error) {
-                                throw error
-                            }
-                            
-                            const accessToken = jwt.sign(user, '123456');
-                            
-                            return res.status(200).json({
-                                response: "comprador added",
-                                access_token: accessToken
-                            })
-                        });
-    
-                    }
-                    const tokenheader = req.headers.authorization  
-                    //supostamente devia decodificar o token mas nao funciona
-                    const tokeninfo = jwt.verify(tokenheader, '123456')
-                    //add a new vendedor to vendedor table
-                    if(type === "vendedor"){
-                        const morada = req.body.morada
-                        const NIF = parseInt(req.body.nif) 
-
-
-                        await pool.query('INSERT INTO vendedor (morada, users_username, nif) VALUES ($1, $2, $3)',[morada,  nome,NIF], (error, result)=>{
-    
-                            if (error) {
-                                throw error
-                            }
-                            const accessToken = jwt.sign(user, '123456');
-                            return res.status(200).json({
-                                response: "vendedor added",
-                                access_token: accessToken
-                            })
-                        });
-                    }
-                    //add a new 
-                    if(type === "administrador"){
-                        await pool.query('INSERT INTO administrador (users_username) VALUES ($1)',[nome], (error, result)=>{
-    
-                            if (error) {
-                                throw error
-                            }
-                            const accessToken = jwt.sign(user, '123456');
-                            return res.status(200).json({
-                                response: "administrador added",
-                                access_token: accessToken
-                            })
-                        });
-                    }
-                });
-
-                
-
-            }
-            
-            else{
-                
-                return res.status(200).json({resultado: "user already exists", user: result.rows[0].username})
-            }
-        })
         
     }
 };
