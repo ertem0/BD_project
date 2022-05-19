96649f273bd3ea64bd6aeed436a1b56df481c737
ertem0 <35893360+ertem0@users.noreply.github.com>
2022-05-19 07:12:50 +0100

    ratings

--- controllers/qandrController.js
+++ controllers/qandrController.js
@@ -33,6 +33,22 @@ module.exports = {
             }
             return res.status(200).json({status: 200, results: result.rows[0]["resposta_id"]})
         })
+    },
+    add_rating: async(req, res)=>{
+        const prod_id = req.params.prod_id;
+        const rating = req.body.rating;
+        const comment = req.body.comment;
+        const autherization= req.headers.authorization;
+
+        const user = jwt.verify(autherization, '123456').username
+        console.log(prod_id, rating, comment, user)
+        pool.query('INSERT INTO ratings (produtos_produto_id, rating, comentario, users_username) VALUES ($1, $2, $3, $4) RETURNING (rating_id)', [prod_id, rating, comment, user] , (error, result)=>{
+            
+            if (error) {
+                return res.status(500).json({err: error})
+                }
+                return res.status(200).json({status: 200, results: result.rows[0]["rating_id"]})
+            })
     }
 }
     
--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,7 @@ const productcontrollers = require('../controllers/productControllers');
 router.route('/user').post(usercontrollers.register).put(usercontrollers.login);
 router.route('/questions/:prod_id').post(qandrcontrollers.create_question);
 router.route('/questions/:prod_id/:question_id').post(qandrcontrollers.create_response);
+router.route('/rating/:prod_id').post(qandrcontrollers.add_rating);
 router.route('/campaign').post(cupoes.createCampanha)
 router.route('/cart').post(productcontrollers.cart);
 router.route('/subscribe/:campanha_id').put(cupoes.subscribe)
