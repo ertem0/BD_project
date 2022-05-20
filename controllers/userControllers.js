505f94ddddcceee1ab20945fcd9291244fae90b2
Rodrigo Santos Figueiredo <rodres2002@gmail.com>
2022-05-20 14:58:57 +0100

    resgist completo

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,15 +22,18 @@ module.exports = {
       if (result.rows[0] === undefined) {
         return res
           .status(400)
-          .json({status: 400, errors: "password incorreta ou user nao existe" });
+          .json({
+            status: 400,
+            errors: "password incorreta ou user nao existe",
+          });
       }
 
       const token = jwt.sign(user, "123456");
       await pool.query("COMMIT");
-      return res.status(200).json({status: 200, token: token});
+      return res.status(200).json({ status: 200, token: token });
     } catch (e) {
       await pool.query("ROLLBACK");
-      return res.status(500).send({status: 500, error: e})
+      return res.status(500).send({ status: 500, error: e });
     }
   },
 
@@ -46,19 +49,21 @@ module.exports = {
     if (tokenheader !== undefined) {
       tokeninfo = jwt.verify(tokenheader, "123456");
     }
-    
+
     try {
       await pool.query("BEGIN");
       let result = await pool.query(
         "SELECT users_username FROM administrador WHERE users_username = $1",
         [tokeninfo.username]
       );
-      
+
       if (
         result.rows[0] === undefined &&
         (type === "administrador" || type === "vendedor")
       ) {
-        return res.status(400).send({status: 400, errors: "user nao é um administrador"});
+        return res
+          .status(400)
+          .send({ status: 400, errors: "user nao é um administrador" });
       }
 
       //gets the table where the username is
@@ -66,78 +71,71 @@ module.exports = {
         "SELECT username FROM users WHERE username = $1",
         [nome]
       );
-      if(result.rows[0] != undefined){
+      if (result.rows[0] != undefined) {
         return res.status(500).json({
           status: 500,
-          errors: "user already exists"
+          errors: "user already exists",
         });
       }
       //check if the return of the select query is empty
-      if (result.rows[0] == undefined) {
-        //add user in the users table
-        
+
+      //add user in the users table
+
+      await pool.query(
+        "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
+        [nome, pass, email]
+      );
+
+      if (type === "comprador") {
+        // get the values for the comprador
+        const morada = req.body.morada;
+        const cartao = req.body.cartao;
+        const NIF = req.body.nif;
+
+        //add comprador in comprador table
         await pool.query(
-          "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
-          [nome, pass, email]
+          "INSERT INTO comprador (morada, cartao, users_username, nif) VALUES ($1, $2, $3, $4)",
+          [morada, cartao, nome, NIF]
         );
-        
-        if (type === "comprador") {
-          // get the values for the comprador
-          const morada = req.body.morada;
-          const cartao = req.body.cartao;
-          const NIF = req.body.nif;
 
-          //add comprador in comprador table
-          await pool.query(
-            "INSERT INTO comprador (morada, cartao, users_username, nif) VALUES ($1, $2, $3, $4)",
-            [morada, cartao, nome, NIF]
-          );
+        await pool.query("COMMIT");
+        return res.status(200).json({
+          status: 200,
+          username: nome,
+        });
+      }
 
-          await pool.query("COMMIT");
-          return res.status(200).json({
-            status: 200,
-            username: nome,
-          });
-        }
-        
-        //add a new vendedor to vendedor table
-        if (type === "vendedor") {
-          
-          const morada = req.body.morada;
-          const NIF = parseInt(req.body.nif);
+      //add a new vendedor to vendedor table
+      if (type === "vendedor") {
+        const morada = req.body.morada;
+        const NIF = parseInt(req.body.nif);
 
-          await pool.query(
-            "INSERT INTO vendedor (morada, users_username, nif) VALUES ($1, $2, $3)",
-            [morada, nome, NIF]
-          );
+        await pool.query(
+          "INSERT INTO vendedor (morada, users_username, nif) VALUES ($1, $2, $3)",
+          [morada, nome, NIF]
+        );
 
-          await pool.query("COMMIT");
-          return res.status(200).json({
-            status: 200,
-            username: nome,
-          });
-        }
-        //add a new
-        if (type === "administrador") {
-          await pool.query(
-            "INSERT INTO administrador (users_username) VALUES ($1)",
-            [nome]
-          );
-          await pool.query("COMMIT");
-          return res.status(200).json({
-            status: 200,
-            username: nome,
-          });
-        } else {
-          return res.status(500).json({
-            status: 500,
-            errors: "user already exists"
-          });
-        }
+        await pool.query("COMMIT");
+        return res.status(200).json({
+          status: 200,
+          username: nome,
+        });
+      }
+      //add a new
+      if (type === "administrador") {
+        await pool.query(
+          "INSERT INTO administrador (users_username) VALUES ($1)",
+          [nome]
+        );
+        await pool.query("COMMIT");
+        return res.status(200).json({
+          status: 200,
+          username: nome,
+        });
       }
     } catch (e) {
       await pool.query("ROLLBACK");
-      return res.status(500).send({status: 500, error: e})
+      return res.status(500).send({ status: 500, error: e });
     }
   },
 };
